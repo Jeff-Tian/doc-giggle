aeaa28ca35dfb4f8ea5af94f6d373a292b5abe5a
Jeff Tian <jie.tian@unicareer.com>
2019-11-23 15:17:30 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+};
+
+export const convert = (docUrl: string) => {
+  return docUrl;
+};
---
+++ test/blah.test.ts
@@ -0,0 +1,18 @@
+import { convert, sum } from '../src';
+
+describe('blah', () => {
+  it('works', () => {
+    expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('converting', () => {
+  it('converts', () => {
+    expect(
+      convert(
+        'https://cdn-global1.unicareer.com/uni-classroom-pc-bff/dev/%E9%A9%AC%E7%8E%89%E7%9A%84%E5%89%AF%E6%9C%AC1574230081124.xlsx'
+      ) ===
+        'https://cdn-global1.unicareer.com/uni-classroom-pc-bff/dev/%E9%A9%AC%E7%8E%89%E7%9A%84%E5%89%AF%E6%9C%AC1574230081124.xlsx'
+    );
+  });
+});
