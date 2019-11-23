990907e96d096e095739a616fa9eda9766ec9405
Jeff Tian <jie.tian@unicareer.com>
2019-11-23 15:50:32 +0800

    feat: convert to buffer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import CloudmersiveConvertApiClient from 'cloudmersive-convert-api-client';
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
@@ -5,6 +8,27 @@ export const sum = (a: number, b: number) => {
   return a + b;
 };
 
-export const convert = (docUrl: string) => {
-  return docUrl;
+const asyncCallback = (resolve: any, reject: any) => (
+  error: any,
+  data: any,
+  _response: any
+) => {
+  if (error) {
+    reject(error);
+  } else {
+    resolve(data);
+  }
+};
+
+export const convert = async (docUrl: string) => {
+  const defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+
+  const Apikey = defaultClient.authentications.Apikey;
+  Apikey.apiKey = '079b7333-32f5-4b7d-8a5b-3af659a6709a';
+
+  const api = new CloudmersiveConvertApiClient.ConvertDocumentApi();
+
+  return await new Promise((resolve, reject) => {
+    api.convertDocumentDocxToPdf(docUrl, asyncCallback(resolve, reject));
+  });
 };
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -7,12 +7,11 @@ describe('blah', () => {
 });
 
 describe('converting', () => {
-  it('converts', () => {
-    expect(
-      convert(
-        'https://cdn-global1.unicareer.com/uni-classroom-pc-bff/dev/%E9%A9%AC%E7%8E%89%E7%9A%84%E5%89%AF%E6%9C%AC1574230081124.xlsx'
-      ) ===
-        'https://cdn-global1.unicareer.com/uni-classroom-pc-bff/dev/%E9%A9%AC%E7%8E%89%E7%9A%84%E5%89%AF%E6%9C%AC1574230081124.xlsx'
+  it('converts', async () => {
+    const res = await convert(
+      'https://cdn-global1.unicareer.com/uni-classroom-pc-bff/dev/%E9%A9%AC%E7%8E%89%E7%9A%84%E5%89%AF%E6%9C%AC1574230081124.xlsx'
     );
+
+    expect(res instanceof Buffer).toBe(true);
   });
 });
