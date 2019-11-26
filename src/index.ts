0db4b93cc16fb4220ffa1bfb448f0503aa46e926
Jeff Tian <jie.tian@unicareer.com>
2019-11-26 13:24:58 +0800

    feat: return buffer

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
-import urllib from "urllib-x";
-import { URL } from "url";
 import request from "request";
 
 export const convert = async (docUrl: string) => {
-  const { res } = await new Promise((resolve, reject) =>
-    request.get(docUrl, { encoding: null }, (err, res) => {
+  const { data } = await new Promise((resolve, reject) =>
+    request.get(docUrl, { encoding: null }, (err, _, data) => {
       if (err) {
         reject(err);
       } else {
-        resolve({ res });
+        resolve({ data });
       }
     })
   );
 
-  return await urllib.request(
-    "https://doc-rotary.herokuapp.com/upload?from=doc-giggle",
-    {
-      files: res,
-      data: {
-        file: new URL(docUrl).pathname.substr(1)
-      },
-      timeout: [10000, 30000]
-    }
-  );
+  const { response } = await new Promise((resolve, reject) => {
+    const req = request.post(
+      `https://doc-rotary.herokuapp.com/upload?from=doc-giggle`,
+      { encoding: null },
+      (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ response });
+        }
+      }
+    );
+
+    const form = req.form();
+    form.append("file", data, {
+      filename: new URL(docUrl).pathname.substr(1)
+    });
+  });
+
+  return response;
 };
--- test/convert.test.ts
+++ test/convert.test.ts
@@ -8,8 +8,8 @@ describe("converting", () => {
       "https://cdn-global1.unicareer.com/uni-classroom-pc-bff/dev/%E9%A9%AC%E7%8E%89%E7%9A%84%E5%89%AF%E6%9C%AC1574230081124.xlsx"
     );
 
-    expect(res.status).toEqual(200);
+    expect(res.statusCode).toEqual(200);
     expect(res.headers["content-type"]).toEqual("application/pdf");
-    expect(res.data[0]).toEqual(37);
+    expect(res.body instanceof Buffer).toBe(true);
   });
 });
